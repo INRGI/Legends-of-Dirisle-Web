8a415cf173a5c4ae450402855c4a3e41b2e4a46b
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-07-06 17:13:24 +0200

    [fix] z-index

--- src/components/ProductModal/ProductModal.styled.tsx
+++ src/components/ProductModal/ProductModal.styled.tsx
@@ -7,7 +7,7 @@ export const Overlay = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 2000;
+  z-index: 99999;
 `;
 
 export const ModalContainer = styled.div`
